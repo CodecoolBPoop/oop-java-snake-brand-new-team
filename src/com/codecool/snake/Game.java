9000f4ced7343bb2644b75307e84cbcb6333de6d
akosVogel <vogelakos@gmail.com>
2018-07-16 22:03:42 +0200

    restart is working

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -3,23 +3,43 @@ package com.codecool.snake;
 import com.codecool.snake.entities.enemies.SimpleEnemy;
 import com.codecool.snake.entities.powerups.SimplePowerup;
 import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
 
 public class Game extends Pane {
 
+    Timeline fiveSecondsWonder;
+
     public Game() {
         new SnakeHead(this, 500, 500);
+        spawnEnemies();
 
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
+    }
 
-        new SimplePowerup(this);
-        new SimplePowerup(this);
-        new SimplePowerup(this);
-        new SimplePowerup(this);
+    public void spawnEnemies() {
+
+        new SimplePowerup(Game.this);
+        new SimpleEnemy(Game.this);
+
+        fiveSecondsWonder = new Timeline(new KeyFrame(Duration.seconds(5), new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                new SimplePowerup(Game.this);
+                new SimpleEnemy(Game.this);
+                }
+        }));
+        fiveSecondsWonder.setCycleCount(Timeline.INDEFINITE);
+        fiveSecondsWonder.play();
+
+
     }
 
     public void start() {
@@ -36,8 +56,25 @@ public class Game extends Pane {
                 case LEFT:  Globals.leftKeyDown  = false; break;
                 case RIGHT: Globals.rightKeyDown  = false; break;
             }
+            if (event.getCode() == KeyCode.R) {
+                restartGame();
+            }
         });
         Globals.gameLoop = new GameLoop();
         Globals.gameLoop.start();
     }
+
+    public void restartGame() {
+        Globals.gameLoop.stop();
+        Globals.gameObjects.clear();
+        Globals.oldGameObjects.clear();
+        Globals.newGameObjects.clear();
+        fiveSecondsWonder.stop();
+        this.getChildren().clear();
+        start();
+        new SnakeHead(this, 500, 500);
+        spawnEnemies();
+
+    }
 }
+
