bd8892db4a1d68b0653e670b4be3231a64bb5853
henrynguyen <henreye@gmail.com>
2018-07-17 13:00:37 +0200

    adds points to score when touches powerup game over popup shows up but after pressing ok game doesnt restart

--- src/com/codecool/snake/entities/snakes/SnakeHead.java
+++ src/com/codecool/snake/entities/snakes/SnakeHead.java
@@ -2,6 +2,7 @@ package com.codecool.snake.entities.snakes;
 
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
+import com.codecool.snake.Game;
 import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
@@ -54,6 +55,8 @@ public class SnakeHead extends GameEntity implements Animatable {
 
         // check for game over condition
         if (isOutOfBounds() || health <= 0) {
+            Game game = new Game();
+            game.gameOver();
             System.out.println("Game Over");
             Globals.gameLoop.stop();
         }
